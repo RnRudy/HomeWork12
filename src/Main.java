76dcf1f964120d0428de01c364797e56cdd58d88
Никита <NVRudy@gmail.com>
2023-08-28 00:59:14 +0300

    hw12

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,35 @@
-import homework13.Author;
-import homework13.Book;
-
-
 public class Main {
     public static void createSpace() {
         System.out.println("~~~~~~~~~~~~~~~");
     }
 
     public static void main(String[] args) {
-        Author author = new Author("Николай Гоголь");
-        Book book = new Book("Мертвые души", author.getName(), 1842);
+        Author author = new Author("Николай ", "Гоголь");
+        Author author1 = new Author("Александр ", "Дюма");
+        Author author2 = new Author("Эрих Мария ", "Ремарк");
+        System.out.println(author.getName());
+        Book book = new Book("Мертвые души", author, 1842);
+        Book book1 = new Book("Граф Монте-Кристо", author1, 1844);
+        Book book2 = new Book("Три товарища", author2, 1936);
         createSpace();
         System.out.println("Название книги: " + book.getBookName());
-        System.out.println("Автор:  " + author.getName());
+        System.out.println("Автор:  " + author.getName() + author.getSurname());
         System.out.println("Год издания: " + book.getYearOfPublishing());
+        book.setYearOfPublishing(1846);
         System.out.println("Год издания (новый): " + book.getYearOfPublishing());
         createSpace();
-        Author author1 = new Author("Александр Дюма");
-        Book book1 = new Book("Граф Монте-Кристо", author1.getName(), 1844);
         System.out.println("Название книги: " + book1.getBookName());
-        System.out.println("Автор: " + author1.getName());
+        System.out.println("Автор: " + author1.getName() + author1.getSurname());
         System.out.println("Год издания: " + book1.getYearOfPublishing());
+        book1.setYearOfPublishing(1950);
         System.out.println("Год издания (новый): " + book1.getYearOfPublishing());
         createSpace();
-        Author author2 = new Author("Эрих Мария Ремарк");
-        Book book2 = new Book("Три товарища", author2.getName(), 1936);
         System.out.println("Название книги: " + book2.getBookName());
-        System.out.println("Автор: " + author2.getName());
+        System.out.println("Автор: " + author2.getName() + author2.getSurname());
         System.out.println("Год издания: " + book2.getYearOfPublishing());
+        book2.setYearOfPublishing(2000);
         System.out.println("Год издания (новый): " + book2.getYearOfPublishing());
-        createSpace();
-        System.out.println("Это одинаковые книги?" + "\nС первой: " + book.equals(book) + "\nСо второй: " + book.equals(book1) + "\nСо третьей: " + book.equals(book2));
-        createSpace();
-        System.out.println("Хеш-коды книг:" + "\nПервой - " + book.hashCode() + "\nВторой - " + book1.hashCode() + "\nТретьей - " + book2.hashCode());
-        createSpace();
-        System.out.println(book);
-        createSpace();
-        System.out.println(book1);
-        createSpace();
-        System.out.println(book2);
+
 
     }
 
